ce13d3097050250836033b159d39f457df386003
orin <dgarci48@jh.edu>
2025-07-20 22:34:14 -0500

    Convert ros_tutorials to regular directory

---
+++ build/turtlesim/turtlesim__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /RoboSub_2026/build/turtlesim/turtlesim__py
+# Build directory: /RoboSub_2026/build/turtlesim/turtlesim__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
