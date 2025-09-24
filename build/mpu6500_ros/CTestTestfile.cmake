31590751f4f10aa7d6f277f27b0e52c0752de268
orin <dgarci48@jh.edu>
2025-09-24 02:52:45 +0000

    removed mpu

--- build/mpu6500_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /RoboSub_2026/src/mpu6500_ros
-# Build directory: /RoboSub_2026/build/mpu6500_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
